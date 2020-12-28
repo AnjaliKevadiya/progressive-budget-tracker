f2873c62a06c8f93e4c9f2fd8151b0892ca2b679
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-12-28 17:06:20 -0500

    specifies all the files to be cached

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,14 @@
 console.log("Hello from service worker!");
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/manifest.json",
+    "/service-worker.js",
+    "/assets/css/style.css",
+    "/assets/images/icons/icon-192x192.png",
+    "/assets/images/icons/icon-512x512.png",
+    "/assets/js/index.js"
+];
+
+const CACHE_NAME = "static-cache-v2",
+const DATA_CACHE_NAME = "data-cache-v1"
